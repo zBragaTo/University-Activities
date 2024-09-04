406d23daf5dba7bd0c13ca6f5777e63025e6dae2
zBragaTo <leonardobragatobassi@hotmail.com>
2024-09-03 23:37:41 -0300

    Atv Concluida

--- src/ED/Aula2.java
+++ src/ED/Aula2.java
@@ -1,85 +1,111 @@
 package ED;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
-//Classe _Aula2_ED: encarregada de executar algumas funcionalidades
-//                  com objetos do tipo Veículos armazenados em ArrayList
-//Autor: Ivan Carlos Alcântara de Oliveira.
-//Data da Criação: 15/08/2024. 15h.
-public class Aula2 {
-	
-	    private ArrayList veic = new ArrayList(); // Inicia o ArrayList
-	    private Scanner ler = new Scanner(System.in); // Objeto de leitura
 
-	    
-	    // Método de entrada do programa
-	    public static void main(String[] args) {
-	    	Aula2 loja = new Aula2();
-	        System.exit(0);
-	    }
-	    
-	    // Construtor da classe _Aula2_ED
-	    // Encarregado de executar as funcionalidades envolvendo  
-	    // objetos da classe Veículo
-	    public Aula2() {
-	        simularLeituraDeDados();
-	        System.out.println("Resultados: \n");
-	        mediaPrecosCaminhoesVendidos();
-	        veiculoMaisCaroMaisBarato();
-	        mediaPrecosDeUmTipoDeVeiculo();
-	        listarTodosOsVeiculosDaLoja();
-	    }
-	    
-	    // Método encarregado de criar objetos da classe Veículo
-	    // e armazenar em um ArrayList.
-	    // Este método foi criado para evitar a necessidade de 
-	    // fazer a leitura de Veículos na fase de testes dos outros ´
-	    // métodos a serem desenvolvidos.
-		public void simularLeituraDeDados() {
-		    veic.add(new Veiculo("ABC-1L34","Honda","GL 1800", 2023, 150000f,"moto")); 
-		    veic.add(new Veiculo("DEF-2K45","Volvo","Scania X", 2015, 179000f,"caminhão")); 
-		    veic.add(new Veiculo("EDF-1P34","Audi","A4", 2019, 234000f,"carro")); 
-		    veic.add(new Veiculo("FGE-2A45","Ford","Focus", 2018, 89000f,"carro")); 
-		    veic.add(new Veiculo("ABD-1B34","Fiat","Track Gls", 2022, 112000f,"carro")); 
-		    veic.add(new Veiculo("DGF-2C45","Volvo","Scania X", 2024, 220000f,"caminhão")); 
-		    veic.add(new Veiculo("FGP-2M45","Ford","Focus", 2021, 120000f,"carro")); 
-		    veic.add(new Veiculo("ABC-1234","Honda","GL 1000", 2024, 140000f,"moto")); 
-		    veic.add(new Veiculo("AEF-2Q45","Volvo","Scania X", 2019, 195000f,"caminhão")); 
-		    veic.add(new Veiculo("IGN-1W45","Ford","Focus", 2020, 110000f,"carro")); 
+public class Aula2 {
+
+	private List<Veiculo> veic = new ArrayList();
+	private Scanner ler = new Scanner(System.in); 
+
+	public static void main(String[] args) {
+		Aula2 loja = new Aula2();
+		System.exit(0);
+	}
+
+	public Aula2() {
+		simularLeituraDeDados();
+		System.out.println("Resultados: \n");
+		mediaPrecosCaminhoesVendidos();
+		veiculoMaisCaroMaisBarato();
+		mediaPrecosDeUmTipoDeVeiculo();
+		listarTodosOsVeiculosDaLoja();
+	}
+	
+	public void simularLeituraDeDados() {
+		veic.add(new Veiculo("ABC-1L34", "Honda", "GL 1800", 2023, 150000f, "moto"));
+		veic.add(new Veiculo("DEF-2K45", "Volvo", "Scania X", 2015, 179000f, "caminhão"));
+		veic.add(new Veiculo("EDF-1P34", "Audi", "A4", 2019, 234000f, "carro"));
+		veic.add(new Veiculo("FGE-2A45", "Ford", "Focus", 2018, 89000f, "carro"));
+		veic.add(new Veiculo("ABD-1B34", "Fiat", "Track Gls", 2022, 112000f, "carro"));
+		veic.add(new Veiculo("DGF-2C45", "Volvo", "Scania X", 2024, 220000f, "caminhão"));
+		veic.add(new Veiculo("FGP-2M45", "Ford", "Focus", 2021, 120000f, "carro"));
+		veic.add(new Veiculo("ABC-1234", "Honda", "GL 1000", 2024, 140000f, "moto"));
+		veic.add(new Veiculo("AEF-2Q45", "Volvo", "Scania X", 2019, 195000f, "caminhão"));
+		veic.add(new Veiculo("IGN-1W45", "Ford", "Focus", 2020, 110000f, "carro"));
+	}
+
+	public void mediaPrecosCaminhoesVendidos() {
+		float somaPrecos = 0;
+		int contadorCaminhoes = 0;
+
+		for (Veiculo veiculo : veic) {
+			if ("caminhão".equalsIgnoreCase(veiculo.getTipo())) {
+				somaPrecos += veiculo.getPreco();
+				contadorCaminhoes++;
+			}
 		}
 
-	// Método que calcula e apresenta a média de preços dos caminhões
-	// Se houver caminhões no ArrayList de Veículos exibe a mensagem:
-	//  "Médias dos preços dos caminhões: R$ ", com duas casas decimais para a média.
-	// Caso não haja caminhões mostra a mensagem: "Não existem caminhões na loja"
-	public void mediaPrecosCaminhoesVendidos() {
-	    // Devenvolver o código Java que realiza o apresentado nos comentários acima.
+		if (contadorCaminhoes > 0) {
+			float media = somaPrecos / contadorCaminhoes;
+			System.out.printf("Média dos preços dos caminhões: R$ %.2f%n", media);
+		} else {
+			System.out.println("Não existem caminhões na loja");
+		}
 	}
 
-	// Método que procura e apresenta o veículo mais caro e mais barato dentro do ArrayList
-	// Se houver Veículos, após a busca, exibe a mensagem:
-	//  "Mais caro: " + <DADOS DO VEÍCULO>
-    //  "Mais barato: " + <DADOS DO VEÍCULO>
-	// Caso não haja veículos cadastrados, mostra a mensagem: "Não há veículos cadastrados!"
 	public void veiculoMaisCaroMaisBarato() {
-	    // Devenvolver o código Java que realiza o apresentado nos comentários acima.	
+
+		if (veic.isEmpty()) {
+			System.out.println("Não há veículos cadastrados");
+		}
+
+		Veiculo veiculoCaro = veic.get(0);
+		Veiculo veiculoBarato = veic.get(0);
+
+		for (Veiculo veiculo : veic) {
+
+			if (veiculo.getPreco() > veiculoCaro.getPreco()) {
+				veiculoCaro = veiculo;
+			}
+			if (veiculo.getPreco() < veiculoBarato.getPreco()) {
+				veiculoBarato = veiculo;
+			}
+		}
+		System.out.println("Mais caro: " + veiculoCaro);
+		System.out.println("Mais barato: " + veiculoBarato);
 	}
 
-	// Método que calcula e apresenta a média de preços de um tipo de veículo (carro, caminhão ou moto)
-	// fornecido pelo usuário.
-	// Se houver veículos no ArrayList de Veículos exibe a mensagem:
-	//  "Médias dos preços dos veículos do tipo " + <TIPO DO VÉICULO> + ": R$ ", com duas casas decimais para a média.
-	// Caso não haja veículos do tipo solicitado, mostra a mensagem: "Não existem veículos desse tipo na loja"	
+
 	public void mediaPrecosDeUmTipoDeVeiculo() {
-	    // Devenvolver o código Java que realiza o apresentado nos comentários acima.	
+	    System.out.println("Digite um veículo para calcular a média de preço deles (carro, caminhão ou moto): ");
+	    String veicuser = ler.next().toLowerCase();  
+
+	    float somaPrecos = 0;
+	    int contador = 0;
+
+	    for (Veiculo veiculo : veic) {
+	        if (veiculo.getTipo().equalsIgnoreCase(veicuser)) {
+	            somaPrecos += veiculo.getPreco();
+	            contador++;
+	        }
+	    }
+	    if (contador > 0) {
+	        float media = somaPrecos / contador;
+	        System.out.printf("Média dos preços dos veículos do tipo %s: R$ %.2f%n", veicuser, media);
+	    } else {
+	        System.out.println("Não existem veículos desse tipo na loja");
+	    }
 	}
 
-	// Método que apresenta todos os dados dos veículos cadastrados.
-	// Se houver veículos no ArrayList exibe a mensagem:
-	//    "Veículos cadastrados:", seguido dos dados dos veículos um por linha.
-	// Caso não haja veículos cadastrados mostra a mensagem: "Não há veículos cadastrados!"
 	public void listarTodosOsVeiculosDaLoja() {
-	    // Devenvolver o código Java que realiza o apresentado nos comentários acima.
+		if (veic.isEmpty()) {
+			System.out.println("Não há veículos cadastrados!");
+		}
+		for (int i = 0; i < veic.size(); i++) {
+			System.out.println(veic.get(i));
+		}
 	}
 }
--- src/ED/Veiculo.java
+++ src/ED/Veiculo.java
@@ -1,23 +1,20 @@
 package ED;
 
 public class Veiculo {
-	// Atributos privados de um veículo
     private String placa, marca, modelo; 
     private int ano; 
     private float preco;
     private String tipo;
 
-    // Construtor sem parâmetros 
     public Veiculo () {
         placa="-"; marca="-"; modelo="-";
         ano=0; preco=0.0f; tipo="-";
     }
-    // Construtor com parâmetros envolvendo todos os 
-    // atributos da classe
+ 
     public Veiculo (String placa, String marca, String modelo,
                     int ano, float preco, String tipo)
     {
-        setPlaca(placa); // ou usar:  this.placa = placa;
+        setPlaca(placa); 
         setMarca(marca); 
         setModelo(modelo);
         setAno(ano); 
@@ -26,95 +23,57 @@ public class Veiculo {
     }
     
     
-    /**
-     * @return the placa
-     */
+
     public String getPlaca() {
         return placa;
     }
 
-    /**
-     * @param placa the placa to set
-     */
     public void setPlaca(String placa) {
         this.placa = placa;
     }
 
-    /**
-     * @return the marca
-     */
     public String getMarca() {
         return marca;
     }
 
-    /**
-     * @param marca the marca to set
-     */
     public void setMarca(String marca) {
         this.marca = marca;
     }
 
-    /**
-     * @return the modelo
-     */
     public String getModelo() {
         return modelo;
     }
 
-    /**
-     * @param modelo the modelo to set
-     */
     public void setModelo(String modelo) {
         this.modelo = modelo;
     }
-
-    /**
-     * @return the ano
-     */
+    
     public int getAno() {
         return ano;
     }
 
-    /**
-     * @param ano the ano to set
-     */
     public void setAno(int ano) {
         this.ano = 0;
         if(ano>1900)this.ano = ano;
     }
 
-    /**
-     * @return the preco
-     */
     public float getPreco() {
         return preco;
     }
 
-    /**
-     * @param preco the preco to set
-     */
     public void setPreco(float preco) {
         this.preco = 0.0f;
         if(preco>0)this.preco = preco;
     }
 
-    /**
-     * @return the tipo
-     */
     public String getTipo() {
         return tipo;
     }
 
-    /**
-     * @param tipo the tipo to set
-     */
     public void setTipo(String tipo) {
         this.tipo = tipo;
     }
-    
-    // Sobrescrita do método toString()
-    // utilizado quando um objeto da classe é 
-    // impresso com System.out.println/print
+
     public String toString() {
         return (getPlaca() 
                 + ",  " + getMarca()  
